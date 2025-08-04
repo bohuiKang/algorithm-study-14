28c329558f90f36d9978ea07b61eab538e2a801d
Ayeong <bay18200725@gmail.com>
2025-08-04 23:58:13 +0900

    ayeong: 9498íí문ì풀이 추가

---
+++ aug_week2/ayeong/ayeong_9498.py
@@ -0,0 +1,15 @@
+score = int(input())
+
+def grade(score):
+    if score > 89:
+        print('A')
+    elif score > 79:
+        print('B')
+    elif score > 69:
+        print('C')
+    elif score > 59:
+        print('D')
+    else:
+        print('F')
+
+grade(score)

aeeb4ec29ad2d3ad8802e065b0cbcd1f26fe1c87
unknown <wldus021128@naver.com>
2025-08-14 17:51:39 +0900

    12927 solved

--- aug_week3/2847_dongjuns_game/jy_2847.py
+++ aug_week3/2847_dongjuns_game/jy_2847.py
@@ -14,24 +14,3 @@ for j in range(n-1, 0, -1):
         count += 1
 
 print(count)
-
-#왜 틀렸는지 모르겠어염
-
-# n = int(input())
-# scores = []
-# scores_goal = []
-# adj_sum = 0
-#
-# for i in range(1, n + 1):
-#     scores.append(int(input()))
-# scores = scores[::-1]
-#
-# for j in range(0, n):
-#     scores_goal.append(scores[-1] - j * 1)
-#
-# for k in range(n):
-#     if scores[k] != scores_goal[k]:
-#         adj = scores[k] - scores_goal[k]
-#         adj_sum += adj
-#
-# print(adj_sum)

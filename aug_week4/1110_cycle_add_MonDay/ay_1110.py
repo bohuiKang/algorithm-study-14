747d891b42c21ec974ab4a184a7b61a218dfc9e2
Ayeong <bay18200725@gmail.com>
2025-08-19 00:23:25 +0900

    ay_1110_solv

--- aug_week4/1110_cycle_add_MonDay/ay_1110.py
+++ aug_week4/1110_cycle_add_MonDay/ay_1110.py
@@ -1,1 +1,14 @@
-ayeong
+N = int(input())
+
+def new_num (num): # 새로운 수 만드는 함수
+    cal_num = (num // 10 + num % 10) % 10 +( num % 10 ) * 10
+    return cal_num
+
+new_cal_num = new_num(N) # 첫 계산
+
+cnt = 1 # 맨처음 계산은 완료 햇으므로 1부터 count (1이 최소 count 1번은 무조건 시행하기때문)
+while  new_cal_num != N: # 입력해준 수와 같아지면 반복문 종료
+    cnt += 1
+    new_cal_num = new_num(new_cal_num)
+
+print(cnt)

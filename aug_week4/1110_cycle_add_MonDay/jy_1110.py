949003389321a040cf7545c2da6c434eacac4cc7
iamp00r <wldus021128@naver.com>
2025-08-19 21:33:56 +0900

    1110  solved

--- aug_week4/1110_cycle_add_MonDay/jy_1110.py
+++ aug_week4/1110_cycle_add_MonDay/jy_1110.py
@@ -1,1 +1,18 @@
-여러분 죄송합니다 시간이 없어서 못했어요 ~! ㅎㅎ
+def make_1(start, end):
+    global cnt, a
+    new_end = str((int(start) + int(end)) % 10)
+    new_start = end
+    new_number = new_start + new_end
+    cnt += 1
+
+    if new_number == a:   # 전역 a와 비교
+        return cnt
+    else:
+        return make_1(new_start, new_end)
+
+a = input().strip()
+if len(a) == 1:
+    a = "0" + a
+
+cnt = 0
+print(make_1(a[0], a[1]))

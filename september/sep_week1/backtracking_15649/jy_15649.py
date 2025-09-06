bcfd6d544ab4d6ac382d693e4e2744b260d959a7
iamp00r <wldus021128@naver.com>
2025-09-06 20:09:58 +0900

    modified

--- september/sep_week1/backtracking_15649/jy_15649.py
+++ september/sep_week1/backtracking_15649/jy_15649.py
@@ -1,1 +1,20 @@
-안풀었음 ㅎㅎ
+from itertools import permutations
+
+N, M = map(int, input().split())
+arr = [i for i in range(1, N+1)]
+
+for p in permutations(arr, M):
+    print(*p)
+
+#---------
+def recur(n, m, lst):
+    if len(lst) == m:
+        print(*lst)
+        return
+
+    for i in range(1, n+1):
+        if i not in lst:
+            recur(n, m, lst + [i])
+
+N, M = map(int, input().split())
+recur(N, M, [])

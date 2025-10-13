ed07ef1572fe58be41e9a1b8e84651be61f3f9e0
bh <lodin356@naver.com>
2025-10-14 00:21:17 +0900

    '집에가고싶어요'

--- 3_october/oct_week2/a-perp_15686_chicken_delivery/bh_15686_chicken_delivery.py
+++ 3_october/oct_week2/a-perp_15686_chicken_delivery/bh_15686_chicken_delivery.py
@@ -1,47 +1,110 @@
-
-def check_distance(sr, sc): # 치킨집 하나와 모든 집의 거리 합
+def check_distance(sr, sc, turn): # 치킨집 하나와 모든 집의 거리 합
+    global distance
     for dr in range(N):
         for dc in range(N):
-            if city[dr][dc] == 1:
-                chicken[idx][1] += abs(sr - dr) + abs(sc - dc)
-
-
-def city_chicken_d(sr, sc): # 집에서 제일 가까운 치킨 거리
-    distance = 0
-    for dr in range(N):
-        for dc in range(N):
-            if city[dr][dc] == 1:
-                pass
-
-    return distance
+            if turn == 1: # 치킨 기준 집 찾기
+                if city[dr][dc] == 1:
+                    chicken[idx][1] += abs(sr - dr) + abs(sc - dc)
+            else: # 집 기준 치킨 찾기
+                if city[dr][dc] == 2:
+                    distance = min(distance, abs(sr - dr) + abs(sc - dc))
+                if distance == 1:
+                    return
 
 
 N, M = map(int, input().split())
 # 0 빈곳, 1 집, 2 치킨집
 city = [list(map(int, input().split())) for _ in range(N)]
-chicken = [[(),0] for _ in range(13)]
+houses = []
+chickens = []
 
-idx = 0
 for r in range(N): # 치킨집을 기준으로 모든 집의 거리 합 저장
     for c in range(N):
-        if city[r][c] == 2: # 치킨집이면,
-            chicken[idx][0] = (r, c)
-            check_distance(r, c) # 인덱스 번호 전달
-            idx += 1 # 치킨 집의 개수 확인 가능
-
-chicken.sort(key= lambda x: x[1], reverse=True)
-
-for i in range(M-idx):  # 치킨집 폐업 시키기
-    closed_r, closed_c = chicken[i][0]
-    city[closed_r][closed_c] = 0
-
-# # 순서 : 좌 상 우 하 좌하 좌상 우상 우하
-# xy = [(0, -1), (-1, 0), (0, 1), (1, 0), (1, -1), (-1, -1), (-1, 1), (1, 1)]
-
-chicken_distance = 0
-for r in range(N): # 도시의 치킨 거리 구하기
-    for c in range(N):
         if city[r][c] == 1: # 집이면,
-            chicken_distance += city_chicken_d(r, c)
+            houses.append((r, c))
+        elif city[r][c] == 2: # 치킨이면,
+            chickens.append((r, c))
 
-print(chicken_distance)
+'''
+def dfs(start, selected):
+    global ans
+
+    # M개의 치킨집을 다 골랐다면 도시 치킨 거리 계산
+    if len(selected) == M:
+        total = 0
+        for hr, hc in houses:
+            dist = 10**9
+            for cr, cc in selected:
+                dist = min(dist, abs(hr - cr) + abs(hc - cc))
+            total += dist
+        ans = min(ans, total)
+        return
+
+    # 아직 다 못 골랐다면 다음 치킨집 선택
+    for i in range(start, len(chickens)):
+        dfs(i + 1, selected + [chickens[i]])
+
+
+# 입력 처리
+N, M = map(int, input().split())
+city = [list(map(int, input().split())) for _ in range(N)]
+
+houses = []
+chickens = []
+for r in range(N):
+    for c in range(N):
+        if city[r][c] == 1:
+            houses.append((r, c))
+        elif city[r][c] == 2:
+            chickens.append((r, c))
+
+ans = float('inf')
+dfs(0, [])
+print(ans)
+'''
+
+
+
+## 논리적 오류
+# def check_distance(sr, sc, turn): # 치킨집 하나와 모든 집의 거리 합
+#     global distance
+#     for dr in range(N):
+#         for dc in range(N):
+#             if turn == 1: # 치킨 기준 집 찾기
+#                 if city[dr][dc] == 1:
+#                     chicken[idx][1] += abs(sr - dr) + abs(sc - dc)
+#             else: # 집 기준 치킨 찾기
+#                 if city[dr][dc] == 2:
+#                     distance = min(distance, abs(sr - dr) + abs(sc - dc))
+#                 if distance == 1:
+#                     return
+#
+#
+# N, M = map(int, input().split())
+# # 0 빈곳, 1 집, 2 치킨집
+# city = [list(map(int, input().split())) for _ in range(N)]
+# chicken = [[(),0] for _ in range(13)]
+#
+# idx = 0
+# for r in range(N): # 치킨집을 기준으로 모든 집의 거리 합 저장
+#     for c in range(N):
+#         if city[r][c] == 2: # 치킨집이면,
+#             chicken[idx][0] = (r, c)
+#             check_distance(r, c, 1) # 인덱스 번호 전달, 집을 찾는다.
+#             idx += 1 # 치킨 집의 개수 확인 가능
+#
+# chicken.sort(key= lambda x: x[1], reverse=True)
+#
+# for i in range(idx-M):  # 치킨집 폐업 시키기
+#     closed_r, closed_c = chicken[i][0]
+#     city[closed_r][closed_c] = 0
+#
+# chicken_distance = 0
+# for r in range(N): # 도시의 치킨 거리 구하기
+#     for c in range(N):
+#         if city[r][c] == 1: # 집이면,
+#             distance = 10000*N # 재할당
+#             check_distance(r, c, 2) # 인덱스 번호 전달, 치킨 집을 찾는다.
+#             chicken_distance += distance
+#
+# print(chicken_distance)

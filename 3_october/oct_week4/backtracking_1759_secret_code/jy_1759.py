1990ec57f996e67ac803162d35f035499c223647
iamp00r <wldus021128@naver.com>
2025-10-28 16:35:07 +0900

    update

--- 3_october/oct_week4/backtracking_1759_secret_code/jy_1759.py
+++ 3_october/oct_week4/backtracking_1759_secret_code/jy_1759.py
@@ -6,10 +6,15 @@ chars = sorted(input().split())
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 
+# 주어진 문자들 중 L개로 만들 수 있는 조합들
 for order in combinations(chars, L):
     vowel_count = 0
+
+    # order(현재 단어 조합)의 철자를 하나씩 보며 모음이면 vowel_count + 1
     for char in order:
         vowel_count += 1 if char in vowels else 0
+    # 자음 = 전체 철자 개수 - 모음 개수
     consonant_count = L - vowel_count
+    # 조건에 만족한다면 출력
     if vowel_count >= 1 and consonant_count >= 2:
         print(*order, sep = '')
--- 3_october/oct_week4/dfs-bfs_2583_find_area/jy_2583.py
+++ 3_october/oct_week4/dfs-bfs_2583_find_area/jy_2583.py
@@ -7,7 +7,9 @@ def bfs(sc, sr):
     global cnt
     q = deque()
     q.append([sc, sr])
+    # 방문한 지역에 대해 arr에 -1 (방문표시)
     arr[sc][sr] = -1
+    # 현재 q에 좌표가 이미 하나 들어갔으므로 temp(각 영역의 넓이)는 1부터 시작
     temp = 1
 
     while q:
@@ -25,12 +27,14 @@ def bfs(sc, sr):
 
 N, M, K = map(int, input().split())
 arr = [[0] * M for _ in range(N)]
+# cnt = 분리되어 나누어지는 영역의 개수
 cnt = 0
 result = []
 
 for _ in range(K):
     r1, c1, r2, c2 = map(int, input().split())
 
+    # 입력 받은 좌표를 이용해 해당 구역에 1씩 더해줌 -> 직사각형이 그려지지 않은 구역은 0이 됨
     for i in range(c1, c2):
         for j in range(r1, r2):
             arr[i][j] += 1
@@ -38,6 +42,7 @@ for _ in range(K):
 
 for i in range(N):
     for j in range(M):
+        # 직사각형이 그려지지 않은 구역에 대해 영역 탐색
         if arr[i][j] == 0:
             bfs(i, j)
 

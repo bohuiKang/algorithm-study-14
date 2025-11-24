76c2532d577d5c4393dbddcd93df18adc0accdc7
chanhwi <kangchan1997@gmail.com>
2025-11-25 01:20:41 +0900

    졸리다

--- 4_november/nov_week4/backtraking_2580_sudoku/ch_2580.py
+++ 4_november/nov_week4/backtraking_2580_sudoku/ch_2580.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 board = [list(map(int, input().split())) for _ in range(9)]
 
 # 빈 칸들 저장
@@ -9,19 +11,19 @@ for i in range(9):
 
 # 빈칸 좌표에 num을 놓을 수 있는지 검사
 def num_check(r, c, num):
-    # 행 검사
+    # 행
     for j in range(9):
         if board[r][j] == num:
             return False
-
-    # 열 검사
+        
+    # 열
     for i in range(9):
         if board[i][c] == num:
             return False
-
-    # 3x3 박스 검사
-    sr = (r // 3) * 3  # 시작 행
-    sc = (c // 3) * 3  # 시작 열
+        
+    # 3x3 박스
+    sr = (r // 3) * 3
+    sc = (c // 3) * 3
     for i in range(sr, sr + 3):
         for j in range(sc, sc + 3):
             if board[i][j] == num:
@@ -30,18 +32,19 @@ def num_check(r, c, num):
     return True
 
 def dfs(idx):
-    # 모든 빈 칸을 다 채웠으면 출력 후 종료
+    # 모든 빈 칸을 다 채웠으면 출력 후 리턴
     if idx == len(blanks):
         for row in board:
             print(*row)
-
+        return True
     r, c = blanks[idx]
 
     # 1 ~ 9까지 넣어보면서 검사
     for num in range(1, 10):
         if num_check(r, c, num):
             board[r][c] = num
-            dfs(idx + 1)
+            if dfs(idx + 1):
+                return True
             board[r][c] = 0  # 원복
 
 dfs(0)
